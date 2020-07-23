cc4587ab77d6eb05ee71a3a4f04975f8c1cbc471
Tzivi <tsivij@gmail.com>
2020-07-23 20:15:10 +0300

    My first solution!

--- exercise-11-1/src/App.js
+++ exercise-11-1/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [typedText, setTypedText] = useState(new String("AA"));
+  
+  function copyText(e) {
+    setTypedText(e.target.value);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <input type="text" value={typedText} onChange={copyText}></input>
+      <input type="text" value={typedText} onChange={copyText}></input>
+      <input type="text" value={typedText} onChange={copyText}></input>
+      <input type="text" value={typedText} onChange={copyText}></input>
+      <input type="text" value={typedText} onChange={copyText}></input>
     </div>
   );
 }
